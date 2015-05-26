bae6dbdf94228c98282f505b0e8ac29b8a1c27d7
AbH Belxjander Draconis Serechai <Belxjander@gmail.com>
2015-05-27 08:11:37 +0900

    REFACTOR:DEBUGGED: finally a decent Hiragana selector...

--- Languages/Japanese/hook.c
+++ Languages/Japanese/hook.c
@@ -146,55 +146,126 @@ ULONG ExecLanguageHook(struct Hook *h,struct LanguageContext *LanguageContext,UL
 	{
 		switch(Message[0])
 		{
-            case TRANSLATE_AMIGA:
-//				KDEBUG("Perception-IME::Japanese.Language::[TRANSLATE_AMIGA:%lx:%lx]\n",Message[1],Message[2]);
-				xc=Message[1];
-				break;
-            case TRANSLATE_ANSI:
+            case TRANSLATE_ANSI:  // Mapped "Vanilla" Keyboard Input
 				Mode=LanguageContext->IPerception->GetLanguageContextAttr(
 					(APTR)LanguageContext,
 					(LONG)LCSTATE_LMODE);
 				Syllable=LanguageContext->IPerception->GetLanguageContextAttr(
 					(APTR)LanguageContext,
 					(LONG)LCSTATE_Syllable);
-				xc=((Message[1] >> 24) & 0x7F);
-				if((xc-0x61)<0x1B)
-					xc=((Message[1] >> 24) & 0x7F);
-				if((xc-0x41)<0x1B)
-					xc=((Message[1] >> 24) & 0x7F)+0x20;
+//				KDEBUG("LOCALE:/Japanese.Language::LanguageHook() Input [ANSI/%lx/%lx]\n", (Message[1] >> 24) & 0x7F, Message[2]);
+				switch((Message[1] >> 24) & 0x7F)
+				{
+					case 0x7A: // z
+					case 0x79: // y
+					case 0x78: // x
+					case 0x77: // w
+					case 0x76: // v
+					case 0x75: // u
+					case 0x74: // t
+					case 0x73: // s
+					case 0x72: // r
+					case 0x71: // q
+					case 0x70: // p
+					case 0x6F: // o
+					case 0x6E: // n
+					case 0x6D: // m
+					case 0x6C: // l
+					case 0x6B: // k
+					case 0x6A: // j
+					case 0x69: // i
+					case 0x68: // h
+					case 0x67: // g
+					case 0x66: // f
+					case 0x65: // e
+					case 0x64: // d
+					case 0x63: // c
+					case 0x62: // b
+					case 0x61: // a
+						xc=(Message[1] >> 24) & 0x7F;
+						break;
+					case 0x5A: // Z
+					case 0x59: // Y
+					case 0x58: // X
+					case 0x57: // W
+					case 0x56: // V
+					case 0x55: // U
+					case 0x54: // T
+					case 0x53: // S
+					case 0x52: // R
+					case 0x51: // Q
+					case 0x50: // P
+					case 0x4F: // O
+					case 0x4E: // N
+					case 0x4D: // M
+					case 0x4C: // L
+					case 0x4B: // K
+					case 0x4A: // J
+					case 0x49: // I
+					case 0x48: // H
+					case 0x47: // G
+					case 0x46: // F
+					case 0x45: // E
+					case 0x44: // D
+					case 0x43: // C
+					case 0x42: // B
+					case 0x41: // A
+						xc=((Message[1] >> 24) & 0x7F)+0x20;
+						break;
+                    case 0x08:
+						Syllable = Syllable >> 8;
+					default:
+						xc=0L;
+						break;
+				}
 				switch(Syllable)
 				{
-					case 0x00:	// Initial State
-						Kana=FindSyllableCandidate(xc,LanguageContext->IUtility);
-						if(Kana==0L)
-							Syllable = (Syllable << 8)+xc;
-						break;
-					case 0x6E:  // N Chord Exception Case
-						switch(xc)
+					case 0x00:
+                        switch(xc)
 						{
 							case 0x61: // A
 							case 0x69: // I
 							case 0x75: // U
 							case 0x65: // E
 							case 0x6F: // O
-								Syllable = (Syllable << 8)+xc;
-								Kana=FindSyllableCandidate(Syllable,LanguageContext->IUtility);
+								Kana=FindSyllableCandidate(xc,LanguageContext->IUtility);
 								break;
-                            default:
-								Kana = 0x00003093;
-								Syllable=xc;
+							default:
+								Syllable=(Syllable << 8)+(xc & 0x7F);
 								break;
 						}
 						break;
-					default:	// Chording
+					case 0x6E:
+                        switch(xc)
+						{
+							case 0x61: // A
+							case 0x69: // I
+							case 0x75: // U
+							case 0x65: // E
+							case 0x6F: // O
+								Kana=FindSyllableCandidate((Syllable << 8)+(xc & 0x7F),LanguageContext->IUtility);
+								break;
+							case 0x79: // Y
+								Syllable=(Syllable << 8)+(xc & 0x7F);
+								break;
+							default:
+								Kana = 0x00003093;
+								Syllable=(xc & 0x7F);
+								break;
+						}
+						break;
+					default:
 						if(Syllable==xc)
 						{
 							Kana = 0x00003063;
 						}else{
-							Syllable = (Syllable << 8)+xc;
-							Kana=FindSyllableCandidate(Syllable,LanguageContext->IUtility);
-							if(Kana)
+                            if(xc)
+							{
+								Syllable=(Syllable << 8)+(xc & 0x7F);
+								Kana=FindSyllableCandidate(Syllable,LanguageContext->IUtility);
+							}else{
 								Syllable=0L;
+							};
 						};
 						break;
 				}
@@ -202,7 +273,25 @@ ULONG ExecLanguageHook(struct Hook *h,struct LanguageContext *LanguageContext,UL
 					(APTR)LanguageContext,
 					(LONG)LCSTATE_Syllable,
 					(LONG)Syllable);
-				KDEBUG("LOCALE:/Japanese.Language::LanguageHook()[ANSI/Syllable=%lx:Kana=%lx]\n",Syllable,Kana);
+				KDEBUG("Japanese.Language Output [ASCII=%lx,CodePoints=%lx:%lx]",Syllable,(Kana >> 16),(Kana & 0xFFFF));
+				break;
+            case TRANSLATE_AMIGA: // Unmapped "Raw" Keyboard Input
+				switch(Message[1]);
+				{
+//	Unofficial Mappings used with Developer Restricted Keymap.Library.Kmod
+					case 0x00000000:	// Perception-IME//Japanese.Language::Hankaku~Zenkaku
+					case 0x78000000:	// Perception-IME//Japanese.Language::Romaji~Hiragana~Katakana
+					case 0x79000000:	// Perception-IME//Japanese.Language::Henkan
+					case 0x7A000000:	// Perception-IME//Japanese.Language::MuHenkan
+//	Official Mappings that need to be recognised
+					case 0x40000000:	// Perception-IME//Japanese.Language::Space
+					case 0x43000000:	// Perception-IME//Japanese.Language::Enter
+					case 0x44000000:	// Perception-IME//Japanese.Language::Return
+					case 0x7F000000:	// Perception-IME//Japanese.Language::Delete
+//  Anything Unhandled as an AMIGA key translation					default:
+						break;
+				}
+//				KDEBUG("Perception-IME::Japanese.Language::[TRANSLATE_AMIGA:%lx:%lx]\n",Message[1],Message[2]);
 				break;
 			default:
 				break;
@@ -211,89 +300,7 @@ ULONG ExecLanguageHook(struct Hook *h,struct LanguageContext *LanguageContext,UL
 
 	return(rc);
 }
-/*
-				switch(xc)
-				{
-//	Unofficial Mappings used with Developer Restricted Keymap.Library.Kmod
-					case	0x00:	//	Change the Constant here for this Key when officially mapped
-					case	0x78:	//	Change the Constant here for this Key when officially mapped
-					case	0x79:	//	Change the Constant here for this Key when officially mapped
-					case	0x7A:	//	Change the Constant here for this Key when officially mapped
-					KDEBUG("Perception-IME//Japanese.Language::Hankaku~Zenkaku\n");
-					KDEBUG("Perception-IME//Japanese.Language::Romaji~Hiragana~Katakana\n");
-					KDEBUG("Perception-IME//Japanese.Language::Henkan\n");
-					KDEBUG("Perception-IME//Japanese.Language::MuHenkan\n");
-//	Official Mappings that need to be recognised
-					case	0x08:	//  Official Key mapping
-					case	0x40:	//  Official Key mapping
-					case	0x43:	//  Official Key mapping
-					case	0x44:	//  Official Key mapping
-					case	0x7F:	//  Official Key mapping
-					KDEBUG("Perception-IME//Japanese.Language::Backspace\n");
-					KDEBUG("Perception-IME//Japanese.Language::Space\n");
-					KDEBUG("Perception-IME//Japanese.Language::Enter\n");
-					KDEBUG("Perception-IME//Japanese.Language::Return\n");
-					KDEBUG("Perception-IME//Japanese.Language::Delete\n");
-//  Anything Unhandled as an AMIGA key translation					default:
-						break;
-				}
-//
-				switch(Syllable)
-				{
-					case 0x00000000: // Initial
-						switch(xc)
-						{
-							case 0x00000061: // A
-							case 0x00000069: // I
-							case 0x00000075: // U
-							case 0x00000065: // E
-							case 0x0000006F: // O
-								Kana=FindSyllableCandidate(xc,LanguageContext->IUtility);
-								break;
-							default:
-								break;
-						}
-						break;
-					case 0x0000006E: // N state
-						switch(xc)
-						{
-							case 0x00000061: // A
-							case 0x00000069: // I
-							case 0x00000075: // U
-							case 0x00000065: // E
-							case 0x0000006F: // O
-							case 0x0000006E: // N
-								Kana=FindSyllableCandidate((Syllable << 8)+(0x7F & xc),LanguageContext->IUtility);
-								Syllable=0L;
-							default:
-								Syllable = (Syllable << 8)+(0x7F & xc);
-								break;
-						}
-						break;
-					default:
-						switch(xc)
-						{
-							case 0x00000061: // A
-							case 0x00000069: // I
-							case 0x00000075: // U
-							case 0x00000065: // E
-							case 0x0000006F: // O
-								Kana=FindSyllableCandidate(
-									(Syllable << 8)+(0x7F & xc),
-									LanguageContext->IUtility);
-								Syllable = 0L;
-							default:
-								if(Syllable==xc)
-								{
-									Kana = 0x00003063;
-								}else{
-									Syllable = (Syllable << 8)+(0x7F & xc);
-								};
-								break;
-						}
-						break;
-				}
-*/
+
 /**/
 ULONG FindSyllableCandidate(ULONG Key, struct UtilityIFace *IUtility)
 {
@@ -304,8 +311,6 @@ ULONG FindSyllableCandidate(ULONG Key, struct UtilityIFace *IUtility)
 		item=IUtility->FindTagItem(TAG_USER|Key,SyllableCandidates);
 	if(item)
 		rc=item->ti_Data;
-
-	KDEBUG("Japanese :: %lx=FindSyllableCandidate(%lx)\n",rc,Key);
 
 	return(rc);
 }
