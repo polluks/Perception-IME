4e4527203b3e250d7a6b5611c261c66a9c2289ff
root <james.william.kerr@gmail.com>
2015-10-08 15:00:00 +0000

    unzipped code points

--- Catalogs/CodePointParser.py
+++ Catalogs/CodePointParser.py
@@ -1,3 +1,13 @@
+import zipfile,os
 
 #open file, create language files etc, iterate line by line
 #identify language and encode code point to correct unicode
+
+"should probably have a more reliable absolute path "
+pathToTopLevelDir = os.path.dirname(os.getcwd())
+CodePointsZipFile = pathToTopLevelDir+"/Resources/Unicode/Unihan.zip"
+
+
+with zipfile.ZipFile(CodePointsZipFile,'r') as allCodePointsZip:
+  allCodePointsZip.extract("Unihan_Readings.txt")
+
