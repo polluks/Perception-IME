f75d3b8776e9f0b3e8bd09ec6d980dc1b3daaea2
root <james.william.kerr@gmail.com>
2015-10-09 10:23:13 +0000

    files output sorted by length

--- Catalogs/CodePointParser.py
+++ Catalogs/CodePointParser.py
@@ -7,43 +7,48 @@ import os
 pathToTopLevelDir = os.path.dirname(os.getcwd())
 CodePointsZipFile = pathToTopLevelDir+"/Resources/Unicode/Unihan.zip"
 
-
-
-
-
-
 allLines = iter(open("Unihan_Readings.txt",'r').read().split('\n'))  #with is a context manager, automatically closes file handles etc no matter what happens
 
 
 def parseJapaneseLine(tokens,lines,dialect):
     codePoint = tokens[0]
     #glyph = blah
-    outfile = "kJapanese"+dialect+".output"
     readings = tokens[2]
-    kJapaneseOutput = open(outfile,"a")
     out = codePoint +" "+readings
-    kJapaneseOutput.write(out+"\n")
-    kJapaneseOutput.close()
     return out
 
+def writeOut(readingsetc,path):
+    output = open(path,"w")
+    for readings in readingsetc:
+        output.write(readings+"\n\n")
+    output.close()
+
+
 def parseJapanese(lines):
-    while lines:
+    kJapaneseKun =[]
+    kJapaneseOn = []
+    line = ""
+
+    while "EOF" not in line:
         line = lines.next()
         if not line.startswith("U"):
           continue
         tokens = line.split("\t")
+
         if "kJapaneseKun" == tokens[1]:
-            parseJapaneseLine(tokens,lines,"kun")
+            kJapaneseKun.append(parseJapaneseLine(tokens,lines,"kun"))
         
         if "kJapaneseOn" == tokens[1]:
-            parseJapaneseLine(tokens,lines,"On")
+            kJapaneseOn.append(parseJapaneseLine(tokens,lines,"On"))
 
-        #if "kJap" in toke
+    kJapaneseOn.sort(key=len,reverse=True)
+    writeOut(kJapaneseOn,"kJapaneseOn.output")
+
+    kJapaneseKun.sort(key=len,reverse=True)
+    writeOut(kJapaneseKun,"kJapaneseKun.output")
+
+    print "\t"+str(len(kJapaneseOn)+len(kJapaneseKun))+" characters processed."
 
 parseJapanese(allLines)
 
 
-def encodeDefinition():pass
-
-
-#languages = {"kcantonese":encodeCantonese,"kDefinition":encodeDefinition,"kJapaneseKun":encodeJapaneseKun}
