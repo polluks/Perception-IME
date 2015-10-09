3e8046dec2ad4a888f624d34b17f9d811cf3583e
root <james.william.kerr@gmail.com>
2015-10-09 09:26:35 +0000

    slowed down by opening and closing files frequently, but conserves memory

--- Catalogs/CodePointParser.py
+++ Catalogs/CodePointParser.py
@@ -1,4 +1,4 @@
-import zipfile,os
+import os
 
 #open file, create language files etc, iterate line by line
 #identify language and encode code point to correct unicode
@@ -8,34 +8,42 @@ pathToTopLevelDir = os.path.dirname(os.getcwd())
 CodePointsZipFile = pathToTopLevelDir+"/Resources/Unicode/Unihan.zip"
 
 
-with zipfile.ZipFile(CodePointsZipFile,'r') as allCodePointsZip:
-  allCodePointsZip.extract("Unihan_Readings.txt")
 
 
 
-with open("Unihan_Readings.txt",'r') as readings: #with is a context manager, automatically closes file handles etc no matter what happens
-  
-  def getLanguages(readings):
-      for line in readings.readlines():
-          print line,"="
-          if "For details on" in line:
-              print "returning"
-              return languages
-          print "found ",line
-          languages.append(line[1:].strip())
-          print languages
+
+allLines = iter(open("Unihan_Readings.txt",'r').read().split('\n'))  #with is a context manager, automatically closes file handles etc no matter what happens
 
 
-  for line in readings.readlines(): #this reads lines into memory one at a  time, so doesnt matter how big the file is.otherwise theres a better way to do this
-      #if "This file contains" in line:
-          #print "this"
-          #languages = getLanguages(readings)
-          #dictionary of language to function
-          if line.startswith('#'):
-              continue
-          tokens = line.split("\t")
-          print tokens
-          cp = tokens[0]
-          language = tokens[1]
-          meaning = tokens[2]
-          print cp, language, meaning
+def parseJapaneseLine(tokens,lines,dialect):
+    codePoint = tokens[0]
+    #glyph = blah
+    outfile = "kJapanese"+dialect+".output"
+    readings = tokens[2]
+    kJapaneseOutput = open(outfile,"a")
+    out = codePoint +" "+readings
+    kJapaneseOutput.write(out+"\n")
+    kJapaneseOutput.close()
+    return out
+
+def parseJapanese(lines):
+    while lines:
+        line = lines.next()
+        if not line.startswith("U"):
+          continue
+        tokens = line.split("\t")
+        if "kJapaneseKun" == tokens[1]:
+            parseJapaneseLine(tokens,lines,"kun")
+        
+        if "kJapaneseOn" == tokens[1]:
+            parseJapaneseLine(tokens,lines,"On")
+
+        #if "kJap" in toke
+
+parseJapanese(allLines)
+
+
+def encodeDefinition():pass
+
+
+#languages = {"kcantonese":encodeCantonese,"kDefinition":encodeDefinition,"kJapaneseKun":encodeJapaneseKun}
