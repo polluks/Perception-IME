8494160309efa8c621f7b31042f32fb4d9a4f916
AbH Belxjander Draconis Serechai <Belxjander@gmail.com>
2015-10-11 23:18:13 +0900

    Updated Python Clearspace rules are mandatory really

--- Catalogs/CodePointParser.py
+++ Catalogs/CodePointParser.py
@@ -3,15 +3,16 @@ import sys,os,time
 allLines = iter(open("Unihan_Readings.txt",'r').read().split('\n')) #iterators are great
 
 def encode(hex):
-    return unichr(int(hex,16))
+	glyph = unichr(int(hex,16))
+	return glyph
 
 def parseDefault(tokens,lines,keyword):
-    readings = tokens[2]
-    return readings
+	readings = tokens[2]
+	return readings
 
 def parseJapanese(tokens,lines,keyword):
-    readings = tokens[2]
-    return readings
+	readings = tokens[2]
+	return readings
 
 parsers = {
 	"Chinese" : parseDefault,
@@ -31,14 +32,13 @@ def parseEverything(lines):
 		tokens = line.split("\t")
 		keyword = tokens[1]
 		keyword = keyword[1:]
-
 		if keyword not in parsers:
 			continue
 		glyph = encode(tokens[0][2:])
 		out = parsers[keyword](tokens,lines)
 		print out
 
-    print "\t"+str(len(kJapaneseOn)+len(kJapaneseKun))+" characters processed."
+	print "\t"+str(len(kJapaneseOn)+len(kJapaneseKun))+" characters processed."
 
 parseEverything(allLines)
 
