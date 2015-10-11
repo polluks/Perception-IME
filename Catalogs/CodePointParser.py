7bec0acf77758e1b3f64b9c5ba1f6abc884742af
AbH Belxjander Draconis Serechai <Belxjander@gmail.com>
2015-10-11 23:04:59 +0900

    Updated CodePointParser.py - stripped it back to essentials

--- Catalogs/CodePointParser.py
+++ Catalogs/CodePointParser.py
@@ -1,84 +1,47 @@
-import os,time
+import sys,os,time
 
-#open file, create language files etc, iterate line by line
-#identify language and encode code point to correct unicode
-
-"should probably have a more reliable absolute path "
-pathToTopLevelDir = os.path.dirname(os.getcwd())
-CodePointsZipFile = pathToTopLevelDir+"/Resources/Unicode/Unihan.zip"
-
-allLines = iter(open("Unihan_Readings.txt",'r').read().split('\n')) #iterators are great 
-verbose = True
+allLines = iter(open("Unihan_Readings.txt",'r').read().split('\n')) #iterators are great
 
 def encode(hex,keyword):
     decimal =int(hex,16)
     codePoint = unichr(decimal)
     return codePoint
 
-"""base parsing line, to be decorated """
-
-def parseLine(tokens,lines,keyword):
-    hex = tokens[0][2:]
-    codePoint = encode(hex,keyword)
+def parseDefault(tokens,lines,keyword):
     readings = tokens[2]
-    readings = readings.decode("utf-8")
-    return codePoint +": "+readings
+    return readings
 
-def japaneseDecoration(parseLine):
-    def innerWrapper(tokens,lines,keyword):
-        print "Decorated."
-        return parseLine(tokens,lines,keyword)
-    return innerWrapper
+def parseJapanese(tokens,lines,keyword):
+    readings = tokens[2]
+    return readings
 
-parseJapaneseLine = japaneseDecoration(parseLine) #move into dict
+parsers = {
+	"Chinese" : parseDefault,
+	"Cantonese" : parseDefault,
+	"JapaneseKun" : parseJapanese,
+	"JapaneseOn" : parseJapanese,
+	"Korean" : parseDefault,
+	"Tang" : parseLine
+}
 
-parsers = {"JapaneseKun" : parseJapaneseLine,"Definition" : parseLine,"Korean" : parseLine,"Tang" : parseLine}
+def parseEverything(lines):
+	line = ""
+	while "EOF" not in line:
+		line = lines.next()
+		if not line.startswith("U"):
+			continue
+		tokens = line.split("\t")
+		keyword = tokens[1]
+		keyword = keyword[1:]
 
-def writeOut(readingsetc,path):
-    output = open(path,"w")
-    for readings in readingsetc:
-        output.write(readings+"\n\n")
-    output.close()
-
-
-def parseJapanese(lines):
-    kJapaneseKun =[]
-    kJapaneseOn = []
-    line = ""
-
-    while "EOF" not in line:
-        line = lines.next()
-        if not line.startswith("U"):
-            continue
-        tokens = line.split("\t")
-        keyword = tokens[1]
-        keyword = keyword[1:]
-       
-        if verbose:
-            #print "processing: ",keyword
-            #print tokens
-            pass
-
-        if keyword not in parsers:
-            continue
-        out = parsers[keyword](tokens,lines,keyword)
-        if verbose:
-            print out
-            #time.sleep(4)
-            #kJapaneseKun.append(parseJapaneseLine(tokens,lines,"kun"))
-        
-        #if "kMandarin" ==tokens[1]:
-        #make dispatcher instead of ifs.keyword is key, lines,tokens,keyword minus k values.
- 
-
-    kJapaneseOn.sort(key=len,reverse=True)
-    writeOut(kJapaneseOn,"kJapaneseOn.output")
-
-    kJapaneseKun.sort(key=len,reverse=True)
-    writeOut(kJapaneseKun,"kJapaneseKun.output")
+		if keyword not in parsers:
+			continue
+		glyph = encode(tokens[0][2:],keyword)
+		out = parsers[keyword](tokens,lines,keyword)
+		print out
 
     print "\t"+str(len(kJapaneseOn)+len(kJapaneseKun))+" characters processed."
 
-parseJapanese(allLines)
+parseEverything(allLines)
 
 
